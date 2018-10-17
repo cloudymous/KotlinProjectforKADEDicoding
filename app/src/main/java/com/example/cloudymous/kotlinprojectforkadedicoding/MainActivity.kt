7e96a9ed42bb1b4664b806c162cd665bb51d1edf
cloudymous <sayacloud@gmail.com>
2018-10-17 09:16:13 +0800

    Buat fungsi untuk button yang proses variable string untuk diganti ke textview

--- app/src/main/java/com/example/cloudymous/kotlinprojectforkadedicoding/MainActivity.kt
+++ app/src/main/java/com/example/cloudymous/kotlinprojectforkadedicoding/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.cloudymous.kotlinprojectforkadedicoding
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,10 +11,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textInput = "Belajar Kotlin"
+        var textInput = "Belajar Kotlin"
 
         val textview = findViewById(R.id.textTV) as TextView
+        val buttonChange = findViewById(R.id.prosesBT) as Button
 
         textview.setText(textInput)
+
+        buttonChange.setOnClickListener {
+            textInput = "Ganti Text"
+            textview.setText(textInput)
+        }
     }
 }
