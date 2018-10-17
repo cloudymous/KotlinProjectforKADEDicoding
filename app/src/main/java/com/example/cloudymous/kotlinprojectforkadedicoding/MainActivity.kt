5cf116eb8eea4d2fc8bf09bfaa17bf6d3985b244
cloudymous <sayacloud@gmail.com>
2018-10-17 08:57:28 +0800

    Coba deklarasi variable textview menggunakan val. Ganti fungsi text jadi settext. Entah mengapa error.

--- app/src/main/java/com/example/cloudymous/kotlinprojectforkadedicoding/MainActivity.kt
+++ app/src/main/java/com/example/cloudymous/kotlinprojectforkadedicoding/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.cloudymous.kotlinprojectforkadedicoding
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textInput = "Belajar Kotlin"
+
+        val textview = findViewById(R.id.textTV) as TextView
+
+        textview.setText(textInput)
     }
 }
