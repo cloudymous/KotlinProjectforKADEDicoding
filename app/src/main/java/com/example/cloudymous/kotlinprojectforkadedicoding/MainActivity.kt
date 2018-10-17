a44bfd77e925aa8725fb94aca67be1a5929d92fe
cloudymous <sayacloud@gmail.com>
2018-10-17 09:18:17 +0800

    Clear untuk belajar variable val (immutable) dan var (mutable)

--- app/src/main/java/com/example/cloudymous/kotlinprojectforkadedicoding/MainActivity.kt
+++ app/src/main/java/com/example/cloudymous/kotlinprojectforkadedicoding/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textInput = "Belajar Kotlin"
+        var textInput = "Belajar Kotlin"
 
         val textview = findViewById(R.id.textTV) as TextView
         val buttonChange = findViewById(R.id.prosesBT) as Button
