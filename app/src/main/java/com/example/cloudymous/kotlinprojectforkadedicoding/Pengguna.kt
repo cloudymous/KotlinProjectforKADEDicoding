b9fcbd2e97c32f05f034e4b11a9daf93f6479911
cloudymous <sayacloud@gmail.com>
2018-10-17 16:44:37 +0800

    Error same JVM Signature

--- app/src/main/java/com/example/cloudymous/kotlinprojectforkadedicoding/Pengguna.kt
+++ app/src/main/java/com/example/cloudymous/kotlinprojectforkadedicoding/Pengguna.kt
@@ -1,2 +1,15 @@
 package com.example.cloudymous.kotlinprojectforkadedicoding
 
+open class Pengguna constructor(val nama: String, val nomorInduk: Int){
+
+    var mNama = nama
+    var mNomorInduk = nomorInduk
+
+    open fun getNama(): String{
+        return mNama
+    }
+
+    open fun getNomorInduk(): Int{
+        return  mNomorInduk
+    }
+}
