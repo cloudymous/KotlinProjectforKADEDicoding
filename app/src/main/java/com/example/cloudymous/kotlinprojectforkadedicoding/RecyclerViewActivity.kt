a0d2dede563d01b4561b255a1e47c8133dc5b5ea
cloudymous <sayacloud@gmail.com>
2018-10-18 10:12:54 +0800

    Tambahkan fungsi untuk menampilkan recycler view

--- app/src/main/java/com/example/cloudymous/kotlinprojectforkadedicoding/RecyclerViewActivity.kt
+++ app/src/main/java/com/example/cloudymous/kotlinprojectforkadedicoding/RecyclerViewActivity.kt
@@ -2,11 +2,36 @@ package com.example.cloudymous.kotlinprojectforkadedicoding
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_recycler_view.*
 
 class RecyclerViewActivity : AppCompatActivity() {
+
+    private var clubs : MutableList<Club> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
+        val list = findViewById<RecyclerView>(R.id.club_list)
+        initData()
+
+        list.layoutManager = LinearLayoutManager(this)
+        list.adapter = RecyclerViewAdapter(this, clubs)
     }
+
+    private fun initData(){
+        val name = resources.getStringArray(R.array.club_name)
+        val image = resources.obtainTypedArray(R.array.club_image)
+        clubs.clear()
+        for (i in name.indices) {
+            clubs.add(Club(name[i],
+                image.getResourceId(i, 0)))
+        }
+
+        //Recycle the typed array
+        image.recycle()
+    }
+
+
 }
